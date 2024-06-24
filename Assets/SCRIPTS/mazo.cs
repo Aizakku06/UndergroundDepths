c3c4a8cc70ab85eb1284af1cc85aa8c66aecbca1
Macko0s <michael.aravenae@estudiantes.arcos.cl>
2024-06-24 18:11:42 -0400

    mazo
    mazo y descarte

---
+++ Assets/SCRIPTS/mazo.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class CartaManager : MonoBehaviour
+{
+    public TextMeshProUGUI cardQuantity;
+    public TextMeshProUGUI discardCardQuantity;
+
+    // Lista para almacenar las cartas
+    [SerializeField] List<GameObject> cartasList = new List<GameObject>();
+    [SerializeField] static List<GameObject> currentCartasList = new List<GameObject>();
+
+    public int cardsInHand = 5;
+    [SerializeField] List<GameObject> manoList = new List<GameObject>();
+    [SerializeField] Transform handContainer;
+
+    [SerializeField] List<GameObject> descarteList = new List<GameObject>();
+
+    public void Start()
+    {
+        if(currentCartasList.Count == 0)
+        {
+            currentCartasList = cartasList;
+        }
+        
+        UpdateText();
+        StartPlayerTurn();
+
+    }
+
+    public void UpdateText()
+    {
+        cardQuantity.text = "" + currentCartasList.Count;
+        discardCardQuantity.text = "" + descarteList.Count;
+    }
+
+    public void StartPlayerTurn()
+    {
+        StartCoroutine(SacarCartas());
+    }
+
+    IEnumerator SacarCartas()
+    {
+        for (int i = 0; i < cardsInHand; i++)
+        {
+            int select = Random.Range(0, currentCartasList.Count);
+            GameObject go =  currentCartasList[select];
+
+            // Hacer anim
+            GameObject cardGO = Instantiate(go);
+            cardGO.transform.SetParent(handContainer);
+            yield return new WaitForSeconds(1f);
+            manoList.Add(go);
+            currentCartasList.RemoveAt(select);
+            UpdateText();
+
+            if(currentCartasList.Count == 0)
+            {
+                for (int j = 0; j < cardsInHand; j++)
+                {
+                    currentCartasList.Add(descarteList[j]);
+                }
+                descarteList.Clear();
+            }
+        }
+
+        yield return new WaitForSeconds(0.02f);
+    }
+
+    public void EndOFTurn()
+    {
+        for (int i = 0; i < cardsInHand; i++)
+        {
+            descarteList.Add(currentCartasList[i]);
+        }
+        currentCartasList.Clear();
+    }
+}
+
