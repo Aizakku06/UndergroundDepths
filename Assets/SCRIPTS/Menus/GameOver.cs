c0e46b64244e2b00b927fb8ea6123ecc77d05ec2
Aizakku06 <169087890+Aizakku06@users.noreply.github.com>
2024-07-18 13:43:26 -0400

    Scene GameOver y Scene Fin arreglada
    menús listos

--- Assets/SCRIPTS/Menus/GameOver.cs
+++ Assets/SCRIPTS/Menus/GameOver.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuGameOver : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
-    [SerializeField] string levelToLoad;
-    [SerializeField] string levelToLoadSecundary;
-    public void Reiniciar()
-    {
-        SceneManager.LoadScene(levelToLoad);
-        SceneManager.LoadScene(levelToLoadSecundary,LoadSceneMode.Additive);
-    }
-
     public void IrAlMenu(string MainMenu)
     {
         SceneManager.LoadScene(MainMenu);
@@ -23,5 +15,4 @@ public class MenuGameOver : MonoBehaviour
         Application.Quit();
         Debug.Log("Aqui se cierra el juego");
     }
-
 }
